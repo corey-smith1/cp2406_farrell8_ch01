c508470636e833b01f9e1b71cd505199132436cf
corey-smith1 <corey_smith661@hotmail.com>
2017-08-01 12:16:55 +1000

    done

--- DebuggingExercises/DebugOne4.java
+++ DebuggingExercises/DebugOne4.java
@@ -1,8 +1,40 @@
 import javax.swing.JOptionPane;
 public class DebugOne4
 {
-  public static main(String[] args)
+  public static void main(String[] args)
   {
-     JOptionPane.showMessageDialog(null, First GUI program)!
+     JOptionPane.showMessageDialog(null, "hey mah boi");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
